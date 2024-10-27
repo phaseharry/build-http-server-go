fb299ccd9a76cf0bcc252f4d02816cf4bd9e382a
phaseharry <harrychenhc@icloud.com>
2024-10-27 18:32:35 -0400

    write back 404 for non-existent path

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 type serverInfo struct {
@@ -33,5 +34,21 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	request := make([]byte, 1024)
+	_, err = connection.Read(request)
+	if err != nil {
+		fmt.Println("Error reading connection request: ", err.Error())
+		os.Exit(1)
+	}
+	requestInfo := strings.Split(string(request), "\r\n")
+
+	requestLine := requestInfo[0]
+	if requestLine != "GET / HTTP/1.1" {
+		connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 404 Not Found\r\n\r\n"))
+		return
+	}
+
+	// fmt.Println(requestString)
 	connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\n\r\n"))
 }
