dd0823555bbc97f3a659ad914b4017e48decb217
phaseharry <harrychenhc@icloud.com>
2024-10-27 19:03:46 -0400

    respond with body of incoming path value

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -28,27 +29,36 @@ func main() {
 		os.Exit(1)
 	}
 
-	// reading only one connection and returning an http 200
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	request := make([]byte, 1024)
-	_, err = connection.Read(request)
-	if err != nil {
-		fmt.Println("Error reading connection request: ", err.Error())
-		os.Exit(1)
-	}
-	requestInfo := strings.Split(string(request), "\r\n")
+		request := make([]byte, 1024)
+		_, err = connection.Read(request)
+		if err != nil {
+			fmt.Println("Error reading connection request: ", err.Error())
+			os.Exit(1)
+		}
+		requestInfo := strings.Split(string(request), "\r\n")
 
-	requestLine := requestInfo[0]
-	if requestLine != "GET / HTTP/1.1" {
-		connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 404 Not Found\r\n\r\n"))
-		return
-	}
+		requestLine := requestInfo[0]
+		requestLineValues := strings.Split(requestLine, " ")
+		method, path, httpVersion := requestLineValues[0], requestLineValues[1], requestLineValues[2]
 
-	// fmt.Println(requestString)
-	connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\n\r\n"))
+		fmt.Printf("method: %v, path: %v, httpVersion: %v", method, path, httpVersion)
+		switch {
+		case method == "GET" && path == "/":
+			connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\n\r\n"))
+		case method == "GET" && strings.HasPrefix(path, "/echo/"):
+			pathValues := strings.Split(path, "/")
+			responseValue := pathValues[len(pathValues)-1]
+			responseSize := strconv.Itoa(len(responseValue))
+			connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + responseSize + "\r\n\r\n" + responseValue))
+		default:
+			connection.Write([]byte("HTTP/" + serverInfo.httpVersion + " 404 Not Found\r\n\r\n"))
+		}
+	}
 }
