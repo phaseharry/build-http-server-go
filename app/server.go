08f8158bcd7eab9eb551573fc8fe70763ceec2a3
phaseharry <harrychenhc@icloud.com>
2024-11-01 21:11:42 -0400

    accept POST request along with body and write contents to file

--- app/server.go
+++ app/server.go
@@ -39,6 +39,7 @@ func handleConnection(conn net.Conn) error {
 	defer conn.Close()
 
 	buffer := make([]byte, 1024)
+
 	_, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading connection request: ", err.Error())
@@ -46,14 +47,13 @@ func handleConnection(conn net.Conn) error {
 	}
 	request := strings.Split(string(buffer), "\r\n")
 
-	for _, val := range request {
-		fmt.Printf("info: %v\n", val)
-	}
+	// for _, val := range request {
+	// 	fmt.Printf("info: %v\n", val)
+	// }
 
 	requestLine, userAgentString := request[0], request[2]
 	requestLineValues := strings.Split(requestLine, " ")
 	method, path, httpVersion := requestLineValues[0], requestLineValues[1], requestLineValues[2]
-	fmt.Printf("userAgentString: %v\n", userAgentString)
 	fmt.Printf("method: %v, path: %v, httpVersion: %v\n", method, path, httpVersion)
 
 	switch {
@@ -62,13 +62,11 @@ func handleConnection(conn net.Conn) error {
 
 	case method == "GET" && strings.HasPrefix(path, "/echo"):
 		responseValue := strings.TrimPrefix(path, "/echo/")
-		fmt.Printf("echo response value: %v", responseValue)
 		responseSize := strconv.Itoa(len(responseValue))
 		conn.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + responseSize + "\r\n\r\n" + responseValue))
 
 	case method == "GET" && strings.HasPrefix(path, "/user-agent"):
 		userAgentValues := strings.Split(userAgentString, " ")
-		fmt.Printf("%v", userAgentValues)
 		userAgent := userAgentValues[len(userAgentValues)-1]
 		responseSize := strconv.Itoa(len(userAgent))
 		conn.Write([]byte("HTTP/" + serverInfo.httpVersion + " 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + responseSize + "\r\n\r\n" + userAgent))
@@ -89,7 +87,6 @@ func handleConnection(conn net.Conn) error {
 		filename := strings.TrimPrefix(path, "/files")
 		directory := os.Args[2]
 		requestBody := request[len(request)-1] // body is always the last part of a request
-		fmt.Printf("%v", requestBody)
 		if err := os.WriteFile(directory+filename, []byte(strings.Trim(requestBody, "\x00")), 0644); err != nil {
 			conn.Write([]byte("HTTP/" + serverInfo.httpVersion + " 500 Error\r\n\r\n"))
 			break
